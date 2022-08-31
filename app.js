53450707ffa19ad28aae90e9e5f398731c6a9b6c
Chong Pei Ying <peiying@MacBook-Pro.local>
2022-08-31 19:21:53 +1000

    inital commit

---
+++ app.js
@@ -0,0 +1,116 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.urlencoded());
+app.use("/js",express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")))
+app.use("/css",express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")))
+app.engine('html',require('ejs').renderFile);
+app.set('view engine','html');
+app.use(express.json())
+app.listen(8080);
+
+app.use(express.static('images'));
+app.use(express.static('css'));
+
+//referencing our schema
+const Parcel = require('./models/parcel');
+const { mainModule } = require('process');
+
+let url = 'mongodb://localhost:27017/parcelDb'
+
+mongoose.connect(url,function(err){
+    if(err){
+        console.log('Error in Mongoose Connection');
+        throw err;
+    }
+    console.log('Succesfully connected')
+})
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,'views/index.html'));
+});
+
+app.get('/addParcel',function(req,res){
+    res.sendFile(path.join(__dirname,'views/addparcel.html'));
+})
+app.post('/parcelAdded', (req, res) => {
+    let newParcel = req.body;
+    newParcel = new Parcel({
+        weight: newParcel.weight,
+        address: newParcel.address,
+        cost: newParcel.cost,
+        sender: newParcel.sender,
+        fragile: newParcel.fragile
+    })
+    newParcel.save(function(err){
+        if(err)throw err;
+        console.log("newParcel successfully added")
+    })
+    res.redirect('/listparcel')
+});  
+
+app.get('/listparcel', function(req,res)
+{
+    Parcel.find({},function(err,docs){
+        if(err) res.json(err);
+        else res.render(__dirname + '/views/list.html', {parcelDb:docs});
+    })
+})
+
+app.get('/deletebyId',function(req,res){
+    res.sendFile(path.join(__dirname,"/views/deletebyId.html"));
+})
+app.get('/deletebySender',function(req,res){
+    res.sendFile(path.join(__dirname,"/views/deletebySender.html"));
+})
+
+app.post('/deletedId',function(req,res){
+    Parcel.deleteOne({'_id': req.body.id},function(err,doc){
+        console.log(doc);
+    })
+    res.redirect('/listparcel')
+})
+
+app.post('/deletedSender',function(req,res){
+    Parcel.deleteMany({'sender': req.body.sender },function(err,doc){
+        console.log(doc);
+    })
+    res.redirect('/listparcel')
+})
+
+app.get('/updateParcel',function(req,res){
+    res.sendFile(path.join(__dirname,'views/updateparcel.html'));
+});
+
+app.post('/updated',function(req,res){
+    Parcel.updateOne({"_id":req.body.id}, {$set: {'sender':req.body.sender,'weight':req.body.weight,"fragile":req.body.fragile,
+    "cost":req.body.cost,"address":req.body.address}},function(err,doc){
+        console.log(doc);
+        res.redirect('/listparcel');
+    })
+})
+
+app.get('/listparcelbySender', function(req,res)
+{
+    res.sendFile(path.join(__dirname,'views/listSenders.html'))
+})
+app.post('/senders',function(req,res){
+    Parcel.find({"sender":req.body.sender},function(err,docs){
+        if(err) res.json(err);
+        else res.render(__dirname + '/views/list.html', {parcelDb:docs});
+    })
+})
+
+app.get('/listparcelbyWeightRange', function(req,res)
+{
+    res.sendFile(path.join(__dirname,'views/listWeight.html'))
+})
+
+app.post('/weightRange',function(req,res){
+    Parcel.find({"weight" :{$gte:req.body.min, $lte: req.body.max}},function(err,docs){
+        if(err) res.json(err);
+        else res.render(__dirname + '/views/list.html', {parcelDb:docs});
+    })
+})
--- models/parcel.js
+++ models/parcel.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 
 let parcelSchema = mongoose.Schema({
-    _id: {type: mongoose.Schema.Types.ObjectId, auto=true},
+    _id: {type: mongoose.Schema.Types.ObjectId, auto:true},
+    weight: Number,
+    fragile: String,
+    address: String,
+    cost: Number,
     sender: { type: String,
             validate: {
                 validator : function(aSender){
                     return aSender.length >=3 
                 },
-                message: 'Sender should be more than 3 characters'}},
-    weight: Number,
-    fragile: String
-
+                message: 'Sender should be more than 3 characters'}}
 })
+module.exports = mongoose.model('Parcel',parcelSchema)
