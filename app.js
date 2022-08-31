9e59ac26a9433a9539f7c4cd34c18eb8955cd896
Chong Pei Ying <peiying@MacBook-Pro.local>
2022-08-31 20:14:37 +1000

    body parser app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }))
 app.use("/js",express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")))
 app.use("/css",express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")))
 app.engine('html',require('ejs').renderFile);
